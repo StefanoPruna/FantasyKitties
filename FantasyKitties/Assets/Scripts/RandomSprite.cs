3f139722e4e2c8681bc44b33a309d9ad70253bd9
StefanoPruna <48112000+StefanoPruna@users.noreply.github.com>
2019-07-09 18:06:17 +1000

    Added environment

---
+++ FantasyKitties/Assets/Scripts/RandomSprite.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RandomSprite : MonoBehaviour
+{
+    public Sprite[] sprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = sprites [Random.Range(0, sprites.Length)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
