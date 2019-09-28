ee8952ee3c7436a270c0ab26fd12b6d55e718132
khaled <khaled.kokah@gmail.com>
2019-09-28 13:52:24 +1000

    added UVScroll script
    UVScroll script for scrolling background

---
+++ FantasyKitties/Assets/Scripts/UVScroll.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UVScroll : MonoBehaviour {
+
+	public Vector2 speed;//Vector2 to make sure you can modify x and y
+
+	void LateUpdate(){
+		GetComponent<Renderer> ().material.mainTextureOffset = speed * Time.time;
+	}
+}
