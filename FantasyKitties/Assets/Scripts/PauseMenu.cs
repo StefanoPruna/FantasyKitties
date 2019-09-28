2c3d9d2feb57d0a9e2b7a1767ddd981374f5795f
khaled <khaled.kokah@gmail.com>
2019-09-28 14:49:59 +1000

    pause button script
    script to use for pausing the gameplay

---
+++ FantasyKitties/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject pauseButton;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Resume()
+    {
+        pauseButton.SetActive(true);
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseButton.SetActive(false);
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        GameManager.isRestart = false;
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Application quit.");
+        Application.Quit();
+    }
+}
