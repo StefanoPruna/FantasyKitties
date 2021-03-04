3e2ce10f61b7af5047303160708035ca34b0ae15
StefanoPruna <48112000+StefanoPruna@users.noreply.github.com>
2021-03-04 15:23:17 +1100

    Updated
    Move the main character at the beginning

---
+++ FantasyKitties/Assets/AimMissiles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimMissiles : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 directionToFace = _Player.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(directionToFace);
+    }
+}
---
+++ FantasyKitties/Assets/EvilMouseMissiles.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvilMouseMissiles : MonoBehaviour
+{
+    public GameObject Missiles;
+    // public GameObject shurikens;
+    [SerializeField]
+    private Transform _Player;
+    public Transform missiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GameObject missile = Instantiate(Missiles, missiles.position);//(GameObject)Instantiate(Shuriken);
+        //missile.transform.position = missiles.transform.position;
+        Vector2 directionToFace = _Player.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(directionToFace);
+    }
+}
